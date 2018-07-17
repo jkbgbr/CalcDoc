eadd39a687931fa6f25c1e9fe3b19314e42479df
JakabG <jkbgbr@gmail.com>
2018-07-17 11:10:09 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CalcDoc',
+    version='0.0.1',
+    packages=['CalcDoc'],
+    url='',
+    license='',
+    author='JakabG',
+    author_email='',
+    description=''
+)
