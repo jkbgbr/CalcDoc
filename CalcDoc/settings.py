ae7658821d61d71235150e258f8174f022fc08bd
JakabG <jkbgbr@gmail.com>
2018-07-17 13:22:31 +0200

    comments

--- CalcDoc/settings.py
+++ CalcDoc/settings.py
@@ -8,9 +8,9 @@ from reportlab.lib.units import mm
 # page size, mirror dimensions
 PAGE_SIZE = 210 * mm, 297 * mm
 LEFT_MARGIN = 25.4 * mm
-TOP_MARGIN = 25.4 * mm
+TOP_MARGIN = 25.4 * mm  # match it with the header height
 RIGHT_MARGIN = 12.7 * mm
-BOTTOM_MARGIN = 11.7 * mm
+BOTTOM_MARGIN = 11.7 * mm  # match it with the footer height
 
-APPNAME = 'perZEUS'
+APPNAME = 'VW'
 VERSION = '0.1 beta'
